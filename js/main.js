a1ac125e461980b76c1c6cb19b36c71107ae54fd
Kacper Kaczmarek <kacper_kaczmarek03@wp.pl>
2022-05-29 22:56:15 +0200

    add scroll script on header. add button in section contact

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-const number = 1;
-const name = "kacper";
+const btn1 = document.querySelector(".read--more");
+
+const scrollMachine = () => {
+  const scrollValue = 720;
+
+  window.scrollTo({
+    top: scrollValue,
+    behavior: "smooth",
+  });
+};
+
+btn1.addEventListener("click", scrollMachine);
