5e9403ea8c9ec4aa93d495ca70488ca2d6a37b5a
Kacper Kaczmarek <kacper_kaczmarek03@wp.pl>
2022-05-30 17:09:44 +0200

    add popup adn effect filter on button in section ofert

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const btn1 = document.querySelector(".read--more");
+const btnPopup = document.querySelector(".popupActive");
 
 const scrollMachine = () => {
   const scrollValue = 720;
@@ -9,4 +10,25 @@ const scrollMachine = () => {
   });
 };
 
+const popUpVisibility = () => {
+  const popup = document.querySelector(".popup");
+  const btnClosePopup = document.querySelector(".exit");
+  let divs = [...document.querySelectorAll("body>div")];
+  divs.pop();
+
+  const closePopup = () => {
+    popup.classList.remove("active");
+    divs.forEach((div) => {
+      div.classList.remove("active");
+    });
+  };
+
+  popup.classList.add("active");
+  divs.forEach((div) => {
+    div.classList.add("active");
+  });
+  btnClosePopup.addEventListener("click", closePopup);
+};
+
 btn1.addEventListener("click", scrollMachine);
+btnPopup.addEventListener("click", popUpVisibility);
